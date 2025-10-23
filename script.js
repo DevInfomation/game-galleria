0ba9867cc2df89bf3971522e8db6276434ad1c08
devinfo <devinformationproject@gmail.com>
2025-10-23 14:28:24 +0200

    feature/game-galleria feat: adding the new feature to the specs box

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ async function displayNewReleases() {
                   </div>
                 </div>
             </div>
-        `;
+          `;
 
     document.querySelector('.another-one').appendChild(newReleases);
   });
@@ -50,27 +50,27 @@ async function displayTopSellers() {
     const topSellers = document.createElement('div');
 
     topSellers.innerHTML = `
-            <div class="catalog-item-container">
-                <div class="product-item--image">
-                  <a href="product-game.html?id=${game.id}"
-                    ><img
-                      src="${game.background_image}"
-                      alt="${game.name}"
-                      width="310px"
-                      height="177.75px"
-                      style="display: block"
-                  /></a>
-                </div>
-                <div class="catalog-item--description">
-                  <div class="catalog-item--title">
-                    <p>${game.name}</p>
-                  </div>
-                  <div class="catalog-item--price">
-                    <p>$${priceCreation()}.99</p>
-                  </div>
-                </div>
+        <div class="catalog-item-container">
+            <div class="product-item--image">
+              <a href="product-game.html?id=${game.id}"
+                ><img
+                  src="${game.background_image}"
+                  alt="${game.name}"
+                  width="310px"
+                  height="177.75px"
+                  style="display: block"
+              /></a>
             </div>
-        `;
+            <div class="catalog-item--description">
+              <div class="catalog-item--title">
+                <p>${game.name}</p>
+              </div>
+              <div class="catalog-item--price">
+                <p>$${priceCreation()}.99</p>
+              </div>
+            </div>
+        </div>
+      `;
 
     document.querySelector('.catalog-list').appendChild(topSellers);
   });
@@ -101,7 +101,103 @@ async function displayGameDetail() {
   const gameDetails = await fetchGameDetailAPI(`games/${gameId}`);
   const { results } = await fetchGameDetailAPI(`games/${gameId}/screenshots`);
 
-  console.log(results);
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+      <h1 class="game-title">${gameDetails.name}</h1>
+      <div class="row-column">
+        <div class="content-container" id="content-container-start">
+          <div class="product-slider-screen">
+            <img src="${gameDetails.background_image}" alt="" class="content-img">
+          </div>
+          <div class="content-sub-container">
+            <div class="content-sub-images">
+              <button class="arrow-btn">
+                <i class="fa-solid fa-less-than"></i>
+              </button>
+              <ul class="preview-container">
+                <li>
+                  <img src="${results[0].image}" alt="" class="content-sub-image-preview">
+                </li>
+                <li>
+                  <img src="${results[1].image}" alt="" class="content-sub-image-preview">
+                </li>
+                <li>
+                  <img src="${results[2].image}" alt="" class="content-sub-image-preview">
+                </li>
+                <li>
+                  <img src="${results[3].image}" alt="" class="content-sub-image-preview">
+                </li>
+                <li>
+                  <img src="${results[4].image}" alt="" class="content-sub-image-preview">
+                </li>
+              </ul>
+              <button class="arrow-btn">
+                <i class="fa-solid fa-greater-than"></i>
+              </button>
+            </div>
+          </div>
+          <div class="product-description">
+            <h4>About this game</h4>
+            <hr>
+            <p class="product-description-text">${gameDetails.description_raw}</p>
+          </div>
+          <div class="product-description product-system-requirements">
+            <h4>System Requirements for PC</h4>
+            <hr>
+            ${
+              gameDetails.platforms[0].requirements.minimum ? `
+                <p>Minimum:</p>
+                <ul class="requirements">
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[1]}</li> 
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[2]}</li> 
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[3]}</li> 
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[4]}</li> 
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[5]}</li> 
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[6]}</li> 
+                  <li>${gameDetails.platforms[0].requirements.minimum.split('\n')[7]}</li> 
+                </ul> 
+              `
+              :
+              `
+                <p>Tags:</p>
+                <ul class="requirements">
+                  ${gameDetails.tags.map(tag => `<li>${tag.name}</li>`).join('')}
+                </ul>
+              `
+            }
+            ${
+              gameDetails.platforms[0].requirements.recommended ? `
+                <p>Recommended:</p>
+                <ul class="requirements">
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[1]}</li>
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[2]}</li>
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[3]}</li>
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[4]}</li>
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[5]}</li>
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[6]}</li>
+                  <li>${gameDetails.platforms[0].requirements.recommended.split('\n')[7]}</li>
+                </ul>
+              ` 
+              :
+              `
+              <p>Genres:</p>
+                <ul class="requirements">
+                  ${gameDetails.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+                </ul>
+              `
+            }
+          </div>
+        </div>
+        <div class="column-content">
+          <!-- the right row -->
+            <!-- <p>test</p> -->
+        </div>
+      </div>
+  `;
+
+  document.getElementById('content-container-start').appendChild(div);
+  
 }
 
 async function fetchGameDetailAPI(endpoint) {
@@ -137,7 +233,7 @@ function init() {
       displayPublishers();
       break;
     case '/product-game.html':
-      // displayGameDetail();
+      displayGameDetail();
       break;
     default:
       console.log('Please enter a proper function');
